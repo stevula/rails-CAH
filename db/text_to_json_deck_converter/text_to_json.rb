1a7f9de59a01f25935473ae5e27279011bdd9563
stevula <brodericksteven@gmail.com>
2016-04-13 20:43:46 -0700

    Convert txt to json successfully

--- db/text_to_json_deck_converter/text_to_json.rb
+++ db/text_to_json_deck_converter/text_to_json.rb
@@ -20,31 +20,30 @@ class TextToJsonDeckConverter
   def convert_deck(input_file_path, output_file_path)
     initialize_json_deck_file(input_file_path, output_file_path)
 
-    existing_deck = json_deck_to_hash(output_file_path)
+    deck          = json_deck_to_hash(output_file_path)
     new_deck_data = txt_deck_to_hash(input_file_path)
-    new_deck_data.each {|k, v| existing_deck[k] = v}
+    new_deck_data.each {|k, v| deck[k] = v}
 
-    output_file = File.open(output_file_path, "w")
-    output_file.puts  new_deck_data.to_json
+    File.open(output_file_path, "w") {|file| file.puts  deck.to_json}
   end
 
   # creates new json deck file if it doesn't already exist
   def initialize_json_deck_file(input_file_path, output_file_path)
     unless File.exist?(output_file_path)
-      deck = {
-        title:       "",
-        series:      "",
-        description: "",
-        black_cards: [],
-        white_cards: []
+      blank_deck = {
+        "title"       => "",
+        "series"      => "",
+        "description" => "",
+        "black_cards" => [],
+        "white_cards" => []
       }
-      json_file = File.open(output_file_path, "w+")
 
-      json_file.puts deck.to_json
+      File.open(output_file_path, "w+") {|file| file.puts blank_deck.to_json}
     end
   end
 
   def json_deck_to_hash(json_file_path)
+    puts json_file_path
     json_deck = File.read(json_file_path)
     JSON.parse(json_deck)
   end
@@ -53,18 +52,18 @@ class TextToJsonDeckConverter
     input_file = File.open(txt_file_path, "r")
 
     if black_or_white(txt_file_path) == :black
-      deck = {black_cards: []}
+      deck_attrs = {"black_cards" => []}
 
       input_file.each.with_index do |text, line_number|
         text = text.chomp
 
         case line_number
         when 0
-          deck[:title] = text
+          deck_attrs["title"]       = text
         when 1
-          deck[:series] = text
+          deck_attrs["series"]      = text
         when 2
-          deck[:description] = text
+          deck_attrs["description"] = text
         # beyond line 2 are all card text lines 
         else
           number_of_blanks = text.scan("______").count
@@ -72,31 +71,30 @@ class TextToJsonDeckConverter
           pick = number_of_blanks > 0 ? number_of_blanks : 1
           draw = pick >= 3 ? pick - 1 : 0
           card =  {
-            text: text,
-            pick: pick,
-            draw: draw
+            "text" => text,
+            "pick" => pick,
+            "draw" => draw
           }
-          deck[:black_cards] << card
+          deck_attrs["black_cards"] << card
         end
       end
     elsif black_or_white(txt_file_path) == :white
-      deck = {white_cards: []}
+      deck_attrs = {"white_cards" => []}
 
       input_file.each.with_index do |text, line_number|
         text = text.chomp
 
-        case line_number
-        when 0
-          deck[:title]       = text
+        if line_number == 0
+          deck_attrs["title"] = text
         # beyond line 0 are all card text lines 
         else
-          card =  {text: text}
-          deck[:white_cards] << card
+          card =  {"text" => text}
+          deck_attrs["white_cards"] << card
         end
       end
     end
 
-    deck
+    deck_attrs
   end
 
   def black_or_white(file_path)
