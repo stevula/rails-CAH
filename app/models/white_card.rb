23966995c01f8310c149c2c371d11daca71458ea
stevula <brodericksteven@gmail.com>
2016-05-05 14:44:17 -0700

    Associate only white cards with discard_pile + related tests/factory changes

--- app/models/white_card.rb
+++ app/models/white_card.rb
@@ -1,1 +1,4 @@
-class WhiteCard < Card; end
+class WhiteCard < Card
+  has_many   :cards_discard_piles, foreign_key: :card_id
+  has_many   :discard_piles, through: :cards_discard_piles
+end
--- spec/factories/discard_piles.rb
+++ spec/factories/discard_piles.rb
@@ -1,5 +1,10 @@
 FactoryGirl.define do
   factory :discard_pile do
-    
+    sequence(:id)
+    game
+
+    after(:build) do |discard_pile|
+      create_list(:white_card, 3, discard_piles: [discard_pile])
+    end
   end
 end
