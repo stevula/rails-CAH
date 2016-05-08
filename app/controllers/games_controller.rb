6d58c8d783537155c5c10dc358a3bc202cd0a79f
stevula <brodericksteven@gmail.com>
2016-05-07 17:06:05 -0700

    Add Game#new/#create/#show

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,4 +1,25 @@
 class GamesController < ApplicationController
   def new
+    @game = Game.new
+  end
+
+  def create
+    @game = Game.new(@game_params)
+
+    if @game.save
+      redirect_to @game
+    else
+      # errors
+    end
+  end
+
+  def show
+    @game = Game.find(params[:id])
+  end
+
+  private
+
+  def game_params
+    params.require(:game).permit(:win_threshold)
   end
 end
